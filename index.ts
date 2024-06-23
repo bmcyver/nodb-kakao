7739e987c2272534b0a9a1c9bd0134234004dd7a
bmcyver <bm.cyver@gmail.com>
2024-06-23 07:13:36 +0000

    Add userId

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const listener = new KakaoDB({
   ip: '127.0.0.1',
   logLevel: LogLevel.DEBUG,
   ignoreError: false,
-  userId: 414336178
+  userId: 'user id here'
 });
 
 listener.on(Events.messageCreate, async (data) => {
